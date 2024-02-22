8073aad93075f84b7752ae6e79ba0e9a8f22b215
gulcin.gedik <gedik.gulcin@gmail.com>
2024-02-22 01:26:07 +0100

    grid column and grid row propogations on the device

--- Eleves_cuda/src/solvers/sudoku_cuda.cu
+++ Eleves_cuda/src/solvers/sudoku_cuda.cu
@@ -14,7 +14,6 @@ sudoku is a carree latin :nah it seems like there is no relation but i am not su
 #include "bitfield.h"
 #include "wfc.h"
 
-
 #include "../inc/helper_cuda.h"
 #include "sudoku_cuda.h" //functions to be called when inside gpu are defined here
 
@@ -25,7 +24,8 @@ solve_cuda(wfc_blocks_ptr blocks)
     printf("ON SOLVER CUDA\n");
     /*first I'll do the sync version*/
     /*then we'lls ee the async*/
-    uint64_t iteration = 0;
+    const uint64_t seed = blocks->states[0]; //with new version we are getting this from outside ??
+    uint64_t iteration  = 0;
     // Set seed
     //const uint64_t seed = blocks->states[0];
     //printf("SEED%lu\n", seed);
@@ -45,7 +45,7 @@ solve_cuda(wfc_blocks_ptr blocks)
     //so the problem below becomes a problem of
     //finding the minimum entropy within the array
     //and keeping the pointer to the structure
-    /*loc.entropy = UINT8_MAX;
+    /*loc.entropy = UINT8_MAX;hop
          for (int gx = 0; gx < blocks->grid_side; gx++)
             for (int gy = 0; gy < blocks->grid_side; gy++)
             {
@@ -76,15 +76,14 @@ solve_cuda(wfc_blocks_ptr blocks)
         //allocate the data space on host and pin it
         checkCudaErrors(cudaMallocHost((void **)&h_blocks, size));
         //initialize the pinned data space
-        memcpy(h_blocks, blocks, size);
+        memcpy(h_blocks, blocks, size); //=> we can get this outside of forever  ?
         //
         wfc_blocks_ptr d_blocks;
         //allocate space on the device
-        checkCudaErrors(cudaMalloc((void **)&d_blocks, size)); //mempitch => is it padded?
+        checkCudaErrors(cudaMalloc((void **)&d_blocks, size)); //mempitch => is it padded? //get this outside of forever ?
         //copy the pinned data from the host to device
-        checkCudaErrors(cudaMemcpy(d_blocks, h_blocks, size, cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(d_blocks, h_blocks, size, cudaMemcpyHostToDevice)); //get this outside of forever ?
 
-       
         entropy_location *dev_entropy_location_per_thread = NULL;
         //it will stock the return fromt the device
         entropy_location *dev_ret_entropy_location_per_thread = NULL;
@@ -100,8 +99,8 @@ solve_cuda(wfc_blocks_ptr blocks)
             dev_entropy_location_per_thread[i]._2               = 0;
         }
 
-        //allocate space in the gpu 
-        checkCudaErrors(cudaMalloc((void**)& dev_ret_entropy_location_per_thread, sizeof(entropy_location)* nb_threads)); 
+        //allocate space in the gpu
+        checkCudaErrors(cudaMalloc((void **)&dev_ret_entropy_location_per_thread, sizeof(entropy_location) * nb_threads)); //get this outside of forever too ? because it will be overwritten each time
         //initialize the allocated space in the gpu
         checkCudaErrors(cudaMemcpy(dev_ret_entropy_location_per_thread, dev_entropy_location_per_thread, sizeof(entropy_location) * nb_threads, cudaMemcpyHostToDevice));
         //make the execution
@@ -109,19 +108,18 @@ solve_cuda(wfc_blocks_ptr blocks)
         //grid information to them => it is known internally
         dev_blk_min_entropy<<<dim_cuda_grid, dim_cuda_block>>>(d_blocks, dev_ret_entropy_location_per_thread);
         //return the array from device to host
-        checkCudaErrors(cudaMemcpy((void **)dev_entropy_location_per_thread, 
-                        dev_ret_entropy_location_per_thread, sizeof(entropy_location) * 
-                        nb_threads, cudaMemcpyDeviceToHost));
+        checkCudaErrors(cudaMemcpy(dev_entropy_location_per_thread,
+                                   dev_ret_entropy_location_per_thread, sizeof(entropy_location) * nb_threads, cudaMemcpyDeviceToHost));
         cudaDeviceSynchronize();
 
-        printf("making sure they are different ? \n"); 
-        for (int i = 0; i < nb_threads; i++) {
-            printf("%" PRIu8 "\n %d %d %d %d \n", dev_entropy_location_per_thread[i].entropy,
-                   dev_entropy_location_per_thread[i].location_in_blk.x,
-                   dev_entropy_location_per_thread[i].location_in_blk.y,
-                   dev_entropy_location_per_thread[i].location_in_grid.x,
-                   dev_entropy_location_per_thread[i].location_in_grid.y);
-        }
+        //printf("making sure they are different ? \n");
+        //for (int i = 0; i < nb_threads; i++) {
+        //    printf("%" PRIu8 "\n %d %d %d %d \n", dev_entropy_location_per_thread[i].entropy,
+        //           dev_entropy_location_per_thread[i].location_in_blk.x,
+        //           dev_entropy_location_per_thread[i].location_in_blk.y,
+        //           dev_entropy_location_per_thread[i].location_in_grid.x,
+        //           dev_entropy_location_per_thread[i].location_in_grid.y);
+        //}
 
         //finding the minimum problem
         for (int i = 0; i < nb_threads; i++) {
@@ -129,36 +127,127 @@ solve_cuda(wfc_blocks_ptr blocks)
             if (test.entropy < loc.entropy)
                 loc = test;
         }
-        grd_print(NULL, blocks); 
-        printf("printing the entropy array minimum: \n");
-        printf("%" PRIu8 "\n %d %d %d %d \n", loc.entropy,
-               loc.location_in_blk.x,
-               loc.location_in_blk.y,
-               loc.location_in_grid.x,
-               loc.location_in_grid.y);
+        //grd_print(NULL, blocks); //last grid encountered
+        //printf("printing the entropy array minimum: \n");
+        //printf("%" PRIu8 "\n %d %d %d %d \n", loc.entropy,
+        //       loc.location_in_blk.x,
+        //       loc.location_in_blk.y,
+        //       loc.location_in_grid.x,
+        //       loc.location_in_grid.y);
         //calling the same function from the cpu solver to compare the results
         //i will not delete them until lost moment! if we have time i will do the streams too and dont want to write the same thing again!
+
+        int h_idx = get_thread_glob_idx(h_blocks,
+                                      loc.location_in_grid.x,
+                                      loc.location_in_grid.y,
+                                      loc.location_in_blk.x,
+                                      loc.location_in_blk.y);
+        printf("index to propagate %d \n ", h_idx); 
+
+        /*it will be called from the cpu
+        since there is only one state to pass i dont see it necessary to do inside gpu*/
+        h_blocks->states[h_idx] = entropy_collapse_state(h_blocks->states[h_idx],
+                                                     loc.location_in_grid.x,
+                                                     loc.location_in_grid.y,
+                                                     loc.location_in_blk.x,
+                                                     loc.location_in_blk.y,
+                                                     seed,
+                                                     iteration);
+                                          /*ABOVE: change in one state*/ 
+        /*rather than copying the whole memory, we can copy only the relevant ones to the right place ?
+        testing will take time so i continue for now*/ 
+        printf(" blocks->states[idx] GPU: %lu\n", h_blocks->states[h_idx]);
+
+        checkCudaErrors(cudaMemcpy(d_blocks, h_blocks, size, cudaMemcpyHostToDevice)); //get this outside of forever ?
+        
+        //block propagate
+                                                                                    //here gotta pass the states from h_blocks, otherwise it wont recognize; it is expecting the value from cpu 
+        dev_blk_propagate<<<dim_cuda_grid, dim_cuda_block>>>(d_blocks, loc.location_in_grid.x,
+                                                             loc.location_in_grid.y, h_blocks->states[h_idx]);
+        
+        
+        //just to print, transferring data 
+        //after verification remove it 
+        //checkCudaErrors(cudaMemcpy(h_blocks,d_blocks, size,cudaMemcpyDeviceToHost));
+        //printf("GPU AFTER BLOCK PROPAGATE\n"); 
+        //grd_print(NULL, h_blocks); 
+
+        dev_grd_propagate_column<<<dim_cuda_grid, dim_cuda_block>>>(d_blocks, loc.location_in_grid.x,
+           loc.location_in_grid.y,  loc.location_in_blk.x, loc.location_in_blk.y,h_blocks->states[h_idx]);
+        //checkCudaErrors(cudaMemcpy(h_blocks,d_blocks, size,cudaMemcpyDeviceToHost));
+        //printf("GPU AFTER COLUMN PROPAGATE\n"); 
+        //grd_print(NULL, h_blocks); 
+
+
+        dev_grd_propagate_row<<<dim_cuda_grid, dim_cuda_block>>>(d_blocks, loc.location_in_grid.x,
+            loc.location_in_grid.y, loc.location_in_blk.x, loc.location_in_blk.y,h_blocks->states[h_idx]);
+       
+       
+        checkCudaErrors(cudaMemcpy(h_blocks,d_blocks, size,cudaMemcpyDeviceToHost));
+        printf("GPU AFTER ROW PROPAGATE\n"); 
+        grd_print(NULL, h_blocks); 
+
+
+
+        /////////CPU///////////////////
         loc.entropy = UINT8_MAX;
-        //it find the minimum entropy for a given grid part, therefore all the things i did above wasnt necessary?! 
-        //    => can we not use it somewhere else 
         for (int gx = 0; gx < blocks->grid_side; gx++)
             for (int gy = 0; gy < blocks->grid_side; gy++) {
                 test = blk_min_entropy(blocks, gx, gy);
                 if (test.entropy < loc.entropy)
                     loc = test;
             }
-        printf("CPU RESULTS\n");
-        printf("%" PRIu8 "\n %d %d %d %d \n", loc.entropy,
-               loc.location_in_blk.x,
-               loc.location_in_blk.y,
-               loc.location_in_grid.x,
-               loc.location_in_grid.y);
+        //printf("CPU RESULTS\n");
+        //printf("%" PRIu8 "\n %d %d %d %d \n", loc.entropy,
+        //       loc.location_in_blk.x,
+        //       loc.location_in_blk.y,
+        //       loc.location_in_grid.x,
+        //       loc.location_in_grid.y);
+        int idx = get_thread_glob_idx(h_blocks,
+                                      loc.location_in_grid.x,
+                                      loc.location_in_grid.y,
+                                      loc.location_in_blk.x,
+                                      loc.location_in_blk.y);
+        blocks->states[idx] = entropy_collapse_state(blocks->states[idx],
+                                                     loc.location_in_grid.x,
+                                                     loc.location_in_grid.y,
+                                                     loc.location_in_blk.x,
+                                                     loc.location_in_blk.y,
+                                                     seed,
+                                                     iteration);
+        printf("index to propagate %d \n ", idx); 
+
+        printf(" blocks->states[idx] CPU: %lu\n", blocks->states[idx]);
+
+        blk_propagate(blocks,
+                      loc.location_in_grid.x,
+                      loc.location_in_grid.y,
+                      blocks->states[idx]);
+        //printf("CPU BLOCK PROPAGATE\n");
+        //grd_print(NULL, blocks);
+        grd_propagate_column(blocks, loc.location_in_grid.x, loc.location_in_grid.y, 
+                            loc.location_in_blk.x, loc.location_in_blk.y, blocks->states[idx]);
+        //printf("CPU AFTER GRID COL PROPAGATE\n"); 
+        //grd_print(NULL, blocks);
+        grd_propagate_row(blocks, loc.location_in_grid.x, loc.location_in_grid.y, 
+                            loc.location_in_blk.x, loc.location_in_blk.y, blocks->states[idx]);
+        printf("CPU AFTER GRID ROW PROPAGATE\n"); 
+        grd_print(NULL, blocks);
+
+        if(verify_states(h_blocks, blocks)){
+            printf("VERIFIED\n");
+        }else {
+            printf("NOT VERIFIED\n"); 
+        }; 
 
         iteration += 1;
         if (iteration == 1) {
             printf("iteration = 1 \n");
             break;
         }
+
+        //free the memory locations from the cpu and gpu!!!
+        //if it's outside of forever call teh destroyer
     }
 
     return false;
--- Eleves_cuda/src/solvers/sudoku_cuda.h
+++ Eleves_cuda/src/solvers/sudoku_cuda.h
@@ -6,6 +6,9 @@ verify_states(wfc_blocks_ptr blocks_ref, wfc_blocks_ptr blocks_test)
     if ((blocks_ref->grid_side != blocks_test->grid_side) ||
         (blocks_ref->block_side != blocks_test->block_side)) {
         printf("gridor block sizes are not the same\n");
+        printf("blocks_ref->grid_side : %u blocks_test->grid_side: %u", blocks_ref->grid_side , blocks_test->grid_side); 
+        printf("blocks_ref->block_side : %u  blocks_test->block_side: %u", 
+        blocks_ref->block_side , blocks_test->block_side); 
         return false;
     }
 
@@ -74,12 +77,11 @@ grd_print_numbers(FILE *const file, const wfc_blocks_ptr block)
 }
 __global__ void
 show_index(wfc_blocks_ptr d_blocks)
-{//for visualizing purposes I am leaving my comments an explaining everything step by step
-    //one thread is getting a block 
-    //therefore it should traverse each element of the block 
+{ //for visualizing purposes I am leaving my comments an explaining everything step by step
+    //one thread is getting a block
+    //therefore it should traverse each element of the block
 
-    
-    //here each thread gets an element of a block 
+    //here each thread gets an element of a block
 
     /*
     int block_size = d_blocks->block_side;
@@ -92,9 +94,9 @@ show_index(wfc_blocks_ptr d_blocks)
     
     d_blocks->states[(row  * width + col)] = row  * width + col; //row*width+col;
     */
-   
-//==> so each thread should leave behind some elemens 
-//here we see that each thread got the first element of a grid
+
+    //==> so each thread should leave behind some elemens
+    //here we see that each thread got the first element of a grid
     /*
     int block_size = d_blocks->block_side;
     int grid_size  = d_blocks->grid_side;
@@ -106,65 +108,70 @@ show_index(wfc_blocks_ptr d_blocks)
     
     d_blocks->states[(row  * width + col)*block_size*block_size] = row  * width + col; //row*width+col;
 */
-   //now each thread takes care of a block 
+    //now each thread takes care of a block
     int block_size = d_blocks->block_side;
     int grid_size  = d_blocks->grid_side;
 
-    int row        = blockIdx.y * blockDim.y + threadIdx.y; // just changed x and y here
-    int col        = blockIdx.x * blockDim.x + threadIdx.x; // just changed x and y here
-    int width      = gridDim.x * blockDim.x;                 // M = a* N
+    int row   = blockIdx.y * blockDim.y + threadIdx.y; // just changed x and y here
+    int col   = blockIdx.x * blockDim.x + threadIdx.x; // just changed x and y here
+    int width = gridDim.x * blockDim.x;                // M = a* N
 
-    for(int i = 0; i < block_size*block_size; i++)
-    d_blocks->states[(row  * width + col)*block_size*block_size +i ] = (row  * width + col)*block_size*block_size +i; //row*width+col;
-
+    for (int i = 0; i < block_size * block_size; i++)
+        d_blocks->states[(row * width + col) * block_size * block_size + i] = (row * width + col) * block_size * block_size + i; //row*width+col;
 }
 
-void see_normal_indexes(wfc_blocks_ptr blocks_ref)
+void
+see_normal_indexes(wfc_blocks_ptr blocks_ref)
 {
-int idx;
+    int idx;
 
-   for (size_t gx = 0; gx < blocks_ref->grid_side; gx++) {
+    for (size_t gx = 0; gx < blocks_ref->grid_side; gx++) {
         for (size_t gy = 0; gy < blocks_ref->grid_side; gy++) {
             for (size_t x = 0; x < blocks_ref->block_side; x++) {
                 for (size_t y = 0; y < blocks_ref->block_side; y++) {
                     idx = get_thread_glob_idx(blocks_ref, gx, gy, x, y);
-                    
-                    blocks_ref->states[idx] = idx; 
+
+                    blocks_ref->states[idx] = idx;
                 }
             }
         }
     }
 }
 
-__global__ void dev_get_thread_glob_idx(){
-
+__device__ void
+dev_get_thread_glob_idx(const wfc_blocks_ptr blocks, uint8_t grid_X, uint8_t grid_Y, uint8_t block_X, uint8_t block_Y, int ret)
+{
+    int block_id = grid_X * blocks->grid_side + grid_Y;
+    int Nb_threads = blocks->block_side * blocks->block_side;
+    int local_id = block_X * blocks->block_side + block_Y;
+    ret =  block_id*Nb_threads + local_id;
 }
 
-__device__ void 
-dev_bitfield_count(uint64_t x, uint8_t* ret)
+__device__ void
+dev_bitfield_count(uint64_t x, uint8_t *ret)
 {
     const uint64_t m1  = 0x5555555555555555; //binary: 0101...
     const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
     const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
     const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
 
-    x -= (x >> 1) & m1;                //put count of each 2 bits into those 2 bits
-    x = (x & m2) + ((x >> 2) & m2);    //put count of each 4 bits into those 4 bits
-    x = (x + (x >> 4)) & m4;           //put count of each 8 bits into those 8 bits
-    (*ret)=  (uint8_t)((x * h01) >> 56); //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+    x -= (x >> 1) & m1;                  //put count of each 2 bits into those 2 bits
+    x      = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+    x      = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+    (*ret) = (uint8_t)((x * h01) >> 56); //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
 }
 
 __device__ void
-dev_entropy_compute(uint64_t state,  uint8_t* ret)
+dev_entropy_compute(uint64_t state, uint8_t *ret)
 {
     dev_bitfield_count(state, ret);
 }
 
-__global__ void 
-dev_blk_min_entropy(const wfc_blocks_ptr blocks, entropy_location* entropy_loctaion_per_thread)
-{//each thread will enter here and will find the location for its own block, 
-//will then stock it in the dedicated case for themselves in the entropy_location* entropy_loctaion_per_thread
-//hmm but it means that the new_loc array then will cointain pointers from gpu    
+__global__ void
+dev_blk_min_entropy(const wfc_blocks_ptr blocks, entropy_location *entropy_loctaion_per_thread)
+{   //each thread will enter here and will find the location for its own block,
+    //will then stock it in the dedicated case for themselves in the entropy_location* entropy_loctaion_per_thread
+    //hmm but it means that the new_loc array then will cointain pointers from gpu
     vec2 blk_location   = { 0 };
     uint8_t min_entropy = UINT8_MAX;
     uint8_t entropy_test;
@@ -173,37 +180,154 @@ dev_blk_min_entropy(const wfc_blocks_ptr blocks, entropy_location* entropy_locta
     int block_size = blocks->block_side;
     int grid_size  = blocks->grid_side;
 
-    int row        = blockIdx.y * blockDim.y + threadIdx.y; // just changed x and y here
-    int col        = blockIdx.x * blockDim.x + threadIdx.x; // just changed x and y here
-    int width      = gridDim.x * blockDim.x;                 // M = a* N
-   
-    //this gives the number of the case that thread starts to read from 
-    int th_idx_arr_location  = (row  * width + col)*block_size*block_size; //therefore it is not thread idx really 
-    int th_idx = row * width + col; 
-    
+    int row   = blockIdx.y * blockDim.y + threadIdx.y; // just changed x and y here
+    int col   = blockIdx.x * blockDim.x + threadIdx.x; // just changed x and y here
+    int width = gridDim.x * blockDim.x;                // M = a* N
+
+    //this gives the number of the case that thread starts to read from
+    int th_idx_arr_location = (row * width + col) * block_size * block_size; //therefore it is not thread idx really
+    int th_idx              = row * width + col;
+
     //Navigate through the block
-    for (int block_x = 0; block_x < blocks->block_side; block_x++)
-        {
+    for (int block_x = 0; block_x < blocks->block_side; block_x++) {
         for (int block_y = 0; block_y < blocks->block_side; block_y++) {
-             idx          =  th_idx_arr_location + block_x  + block_y;
-             dev_entropy_compute(blocks->states[idx], &entropy_test);
+            idx = th_idx_arr_location + block_x + block_y; //it should be  block_x * block_size + block_y; ?? but it works ?? gotta check
+            dev_entropy_compute(blocks->states[idx], &entropy_test);
             if ((entropy_test < min_entropy) && (entropy_test > 1)) {
                 min_entropy    = entropy_test;
                 blk_location.x = block_x;
                 blk_location.y = block_y;
             }
         }
-        }
-    
-    int our_grid_Idx = blockIdx.x / grid_size; 
+    }
+
+    int our_grid_Idx = blockIdx.x / grid_size;
     int our_grid_Idy = blockIdx.x % grid_size;
 
     entropy_loctaion_per_thread[th_idx].entropy            = min_entropy;
     entropy_loctaion_per_thread[th_idx].location_in_blk    = blk_location;
-    entropy_loctaion_per_thread[th_idx].location_in_grid.x = our_grid_Idx;  
-    entropy_loctaion_per_thread[th_idx].location_in_grid.y = our_grid_Idy; 
-
-
-
+    entropy_loctaion_per_thread[th_idx].location_in_grid.x = our_grid_Idx;
+    entropy_loctaion_per_thread[th_idx].location_in_grid.y = our_grid_Idy;
     //return new;
+}
+
+__global__ void
+dev_blk_propagate(wfc_blocks_ptr blocks,
+                  uint32_t gx, uint32_t gy,
+                  uint64_t collapsed)
+{
+    
+    /*if the thread is not owner the block with the minimum entropy it is not going to propagate*/
+    int block_size   = blocks->block_side;
+    int grid_size    = blocks->grid_side;
+    int our_grid_Idx = blockIdx.x / grid_size;
+    int our_grid_Idy = blockIdx.x % grid_size;
+    if (our_grid_Idx != gx || our_grid_Idy != gy) {
+        return; //do nothing and return
+        //or should i synchronise the threads and they ar egoing to wiat for the executing thread?
+    }
+    int row   = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col   = blockIdx.x * blockDim.x + threadIdx.x; 
+    int width = gridDim.x * blockDim.x;                
+
+    //this gives the number of the case that thread starts to read from
+    int th_idx_arr_location = (row * width + col) * block_size * block_size; 
+    int th_idx              = row * width + col;
+    int idx;
+    uint8_t bc;
+    for (int i = 0; i < blocks->block_side; i++)
+        for (int j = 0; j < blocks->block_side; j++) {
+            idx = th_idx_arr_location + i*blocks->block_side + j;
+            dev_bitfield_count(blocks->states[idx], &bc);
+            
+            if (bc != 1) 
+                blocks->states[idx] &= ~(collapsed);
+        }
+}
+
+__global__ void                                 //given grid indexes 
+dev_grd_propagate_column(wfc_blocks_ptr blocks, uint32_t gx, uint32_t gy,
+                     uint32_t x, uint32_t y, uint64_t collapsed)
+{
+    //each thread gets a block 
+    //for grid propopagate column it means that each thread will update the the its rows on that column 
+    //based on the given state 
+    //but not every grid is going to do this! only he grids that are on the same column 
+    //as the given grid is going to do this!! 
+
+    int block_size   = blocks->block_side;
+    int grid_size    = blocks->grid_side;
+    int our_grid_Idx = blockIdx.x / grid_size;
+    int our_grid_Idy = blockIdx.x % grid_size;
+    int row   = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col   = blockIdx.x * blockDim.x + threadIdx.x; 
+    int width = gridDim.x * blockDim.x;                
+    int th_idx_arr_location = (row * width + col) * block_size * block_size; 
+    int th_idx              = row * width + col;
+    int grd_size_mod = th_idx % grid_size; //=> will determine in which block we are
+    int needed_grid_index = gx * grid_size + gy; 
+    int needed_grid_index_mod = needed_grid_index% grid_size; 
+    if (grd_size_mod !=  needed_grid_index_mod){ //if not on the same column don't do somethin
+        return; 
+    }
+
+    if (th_idx == needed_grid_index){ //we have already propagated on this column in block
+        return; 
+    }
+    int idx;
+    uint8_t bc;
+
+    for (int i = 0; i < blocks->block_side; i++)
+    {
+        //for (int gxx = 0; gxx < blocks->grid_side; gxx++) { //dont traverse the grid side because each thread has a block, it does it naturally
+             idx = th_idx_arr_location + i* block_size  + y; 
+            dev_bitfield_count(blocks->states[idx], &bc);
+            if (bc != 1) 
+                blocks->states[idx] &= ~(collapsed);
+    }
+                
+}
+
+
+__global__ void
+dev_grd_propagate_row(wfc_blocks_ptr blocks,
+                  uint32_t gx, uint32_t gy, uint32_t x, uint32_t y,
+                  uint64_t collapsed)
+{
+    int block_size   = blocks->block_side;
+    int grid_size    = blocks->grid_side;
+    int our_grid_Idx = blockIdx.x / grid_size; //rox location in the grid
+    int our_grid_Idy = blockIdx.x % grid_size; //col location in the grid 
+    int row   = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col   = blockIdx.x * blockDim.x + threadIdx.x; 
+    int width = gridDim.x * blockDim.x;                
+    int th_idx_arr_location = (row * width + col) * block_size * block_size; 
+    int th_idx              = row * width + col;
+    int grd_size_mod = th_idx % grid_size; //=> will determine in which block we are
+    int needed_grid_index = gx * grid_size + gy; 
+    int needed_grid_index_mod = needed_grid_index% grid_size; 
+    int idx;
+    uint8_t bc;
+
+    //dont do if on a different row 
+
+    //dont do if the grid is the asked grid 
+    if (th_idx == needed_grid_index){ //we have already propagated on this column in block
+        return; 
+    }
+
+//do only if on the same row 
+if(our_grid_Idx == gx ){ 
+
+
+    for (int j = 0; j < blocks->block_side; j++)
+        {
+           
+            idx = th_idx_arr_location + x* block_size  + j; 
+             dev_bitfield_count(blocks->states[idx], &bc);
+            if (bc != 1)
+                blocks->states[idx] &= ~(collapsed);
+        }
+}
+    // return 0;
 }
