e98dba044ea221079b6d96ab4a47cb6a7607b55f
Pacome Rousselle <pacomerouselle@gmail.com>
2024-02-25 13:01:31 +0100

    First try

--- Eleves_cuda/src/solvers/sudoku_omp.cu
+++ Eleves_cuda/src/solvers/sudoku_omp.cu
@@ -6,7 +6,98 @@
 #include <omp.h>
 
 bool
-solve_openmp(wfc_blocks_ptr, uint64_t seed, masks*)
+solve_openmp(wfc_blocks_ptr blocks, uint64_t seed, masks* my_masks)
 {
-    return false;
+    printf("inside solver omp \n"); 
+        //print_masks(my_masks, blocks->block_side, blocks->grid_side);
+    grd_print(NULL, blocks);
+    // seed = blocks->states[0];
+
+    uint64_t iteration  = 0;
+
+    entropy_location loc, test;
+    int changed = 0; 
+    /*printing the current stuation in the beginning*/
+
+    //printf("grid side: %u\n", blocks->grid_side); 
+    //printf("block side: %u\n", blocks->block_side); 
+    
+    uint32_t col_idx  ;
+    uint32_t row_idx  ;
+    uint32_t block_idx;
+    #pragma omp parallel for num_threads(blocks->grid_side*blocks->grid_side)
+    forever {
+        //printf("Itération %d\n",iteration);
+        //while find_min_entropy in grid 
+        loc.entropy = UINT8_MAX;
+        test = blk_min_entropy(blocks,0,omp_get_thread_num());
+        if(test.entropy < loc.entropy)
+            loc = test;
+        if (loc.entropy == UINT8_MAX)
+            break;        
+        
+        //printf("loc.entropy is %zu\n", loc.entropy); 
+        if (loc.entropy == 1)
+        {
+        //printf("inside loc.entropy = 1\n"); 
+            return false;
+        }
+        printf("before collapsing\n"); 
+        grd_print(NULL, blocks); 
+        //print_masks(my_masks, blocks->block_side, blocks->grid_side);
+
+        // Collapse
+        int idx = get_thread_glob_idx(blocks,
+                                      0,
+                                      omp_get_thread_num(),
+                                      loc.location_in_blk.x,
+                                      loc.location_in_blk.y);
+        //printf("idx: %d\n", idx); 
+        blocks->states[idx] = entropy_collapse_state(blocks->states[idx],
+                                                    0,
+                                                    omp_get_thread_num(),
+                                                    loc.location_in_blk.x,
+                                                    loc.location_in_blk.y,
+                                                    seed,
+                                                    iteration);
+        col_idx   = omp_get_thread_num() * blocks->block_side + loc.location_in_blk.y; 
+        row_idx   = 0 * blocks->block_side + loc.location_in_blk.x; 
+        block_idx = 0 * blocks->grid_side  + omp_get_thread_num(); 
+        printf("after collapsing\n"); 
+        grd_print(NULL, blocks); 
+        set_mask(my_masks, col_idx, row_idx, block_idx, blocks->states[idx]); 
+        if(my_masks->safe_exit == 1)
+        {
+            printf("conflict while collapsing, safe exiting\n"); 
+            return false; 
+        }
+        //print_masks(my_masks, blocks->block_side, blocks->grid_side);
+        // Propagate
+        // In the block
+        //printf("Before propagate\n"); 
+        //grd_print(NULL, blocks);
+        
+        if(propagate_all(blocks,
+                      0,
+                      omp_get_thread_num(),
+                      loc.location_in_blk.x,
+                      loc.location_in_blk.y,
+                      blocks->states[idx], my_masks) == false) {
+                        
+                        
+                        return false; } 
+        grd_print(NULL, blocks); 
+        
+        print_masks(my_masks, blocks->block_side, blocks->grid_side);
+
+        iteration += 1;
+        //if (iteration == 1) break; 
+    }
+
+    //print the last state 
+    printf("last state: \n"); 
+    grd_print(NULL, blocks); 
+
+    // return D
+    return true;
 }
